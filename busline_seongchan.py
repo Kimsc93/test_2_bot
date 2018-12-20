034f22ce346fbd8ff61a16af53cc89d98d299ce5
Kimsc93 <rtc0822@naver.com>
2018-12-20 15:59:12 +0900

    config.json
    change_token

--- busline_seongchan.py
+++ busline_seongchan.py
@@ -1,20 +1,23 @@
-def _crawl_naver_keywords(text):
-    text = re.sub(r'<@\S+> ', '', text) #아이디 떼는 코드
+import re
+import urllib.request
+import json
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from slackclient import SlackClient
+from flask import Flask, request, make_response, render_template
+
+driver = webdriver.Chrome(r'C:\Users\student\Desktop\chromedriver_win32\chromedriver.exe')
+
+def busline(text):
+    # 웹사이트 열기
+    driver.get("https://bus.gwangju.go.kr/busmap/lineSearch")
+    bt = driver.find_element_by_css_selector("button.btn.btn-default")  # 클래스만 있는경우
+    bt.click()
+
     url = "http://www.jobkorea.co.kr/Salary/"
     source = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(source, "html.parser")
     company = soup.find_all("div", class_="slide")
-
-    i = 0
-
-    if "대기업" in text:
-        i = 0
-    elif "중견기업" in text:
-        i = 1
-    elif "공기업" in text:
-        i = 2
-    else:
-        return u'it is not valid url.'
 
     result = company[i].get_text()[:-10].replace("\n\n\n\n", "\n").replace("\n\n", "\n").replace("\n\n", " ")
 
--- main.py
+++ main.py
@@ -23,27 +23,27 @@ sc = SlackClient(slack_token)
 
 # 크롤링 함수 구현하기
 def _crawl_naver_keywords(text):
-    # text = re.sub(r'<@\S+> ', '', text) #아이디 떼는 코드
-    #
-    # url = "http://www.jobkorea.co.kr/Salary/"
-    # source = urllib.request.urlopen(url).read()
-    # soup = BeautifulSoup(source, "html.parser")
-    # company = soup.find_all("div", class_="slide")
-    #
-    # i = 0
-    #
-    # if "대기업" in text:
-    #     i = 0
-    # elif "중견기업" in text:
-    #     i = 1
-    # elif "공기업" in text:
-    #     i = 2
-    # else:
-    #     return u'it is not valid url.'
-    #
-    # result = company[i].get_text()[:-10].replace("\n\n\n\n", "\n").replace("\n\n", "\n").replace("\n\n", " ")
+    text = re.sub(r'<@\S+> ', '', text) #아이디 떼는 코드
 
-    return "asdasd"#result
+    url = "http://www.jobkorea.co.kr/Salary/"
+    source = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(source, "html.parser")
+    company = soup.find_all("div", class_="slide")
+
+    i = 0
+
+    if "대기업" in text:
+        i = 0
+    elif "중견기업" in text:
+        i = 1
+    elif "공기업" in text:
+        i = 2
+    else:
+        return u'it is not valid url.'
+
+    result = company[i].get_text()[:-10].replace("\n\n\n\n", "\n").replace("\n\n", "\n").replace("\n\n", " ")
+
+    return result
 
 
 def _event_handler(event_type, slack_event):
