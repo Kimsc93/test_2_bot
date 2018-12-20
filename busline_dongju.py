20ea301299f894146a1d3eb610f76f0e0429dce4
kimdongju <ilil26@naver.com>
2018-12-20 16:24:56 +0900

    dd

--- busline_dongju.py
+++ busline_dongju.py
@@ -8,11 +8,19 @@ from slackclient import SlackClient
 from flask import Flask, request, make_response, render_template
 
 def notification():
-    text = re.sub(r'<@\S+> ', '', text)  # 아이디 떼는 코드
     url = "http://bus.gjcity.net/guide/notice/noticeList"
     source = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(source, "html.parser")
-    company = soup.find_all("div", class_="slide")
+
+    # print(soup)
+    subjects = []
+    num = []
+    # date=[]
+    for subject in soup.find_all("a", class_="txt_pt13"):
+        subjects.append(subject.get_text())
+    for number in soup.find_all("td", class_="listtd txt_pt13"):
+        num.append(number.get_text())
+    return subjects
 
 
 
