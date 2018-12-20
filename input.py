ff8d03d630dcaf1f3151e14338e36ea8e0e4e818
GwonTaeYong <taaii6569@naver.com>
2018-12-20 16:10:52 +0900

    taeyong
    update to check bus stop option

--- input.py
+++ input.py
@@ -8,20 +8,36 @@ import re
 0 = 다시 입력
 1 = 버스노선 검색
 2 = 공지사항 출력
+3 = 정거장 확인
 
 '''
+
+CHECK = ["정거", "정류"]
+RESULT =["다시입력", "버스노선 검색", "공지사항 출력", "정거장 확인"]
+
 def input_pre_processing(user_input):
     numbers = re.findall("\d+", user_input)
 
+
     if len(numbers) > 1:
         result = 0
-    elif numbers == 1:
+    elif len(numbers) == 1:
         result = 1
     else:
+        for word in CHECK:
+            if word in user_input:
+                result = 3
+                return result
         result = 2
 
     return result
 
 if __name__ == "__main__":
     input_ = "37번 버스 노선 알려줘111"
-    print(input_pre_processing(input_))
+    print(RESULT[input_pre_processing(input_)])
+    input_1 = "11번 버스 노선 알려줘!"
+    print(RESULT[input_pre_processing(input_1)])
+    input_2 = "공지사항 알려줘"
+    print(RESULT[input_pre_processing(input_2)])
+    input_3 = "정광고 버스 정류장 상황 알려줘"
+    print(RESULT[input_pre_processing(input_3)])
