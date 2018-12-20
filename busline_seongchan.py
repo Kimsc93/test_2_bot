6709d6a49bc3192ee254e918bbc1448badb7eb88
Kimsc93 <rtc0822@naver.com>
2018-12-20 16:46:10 +0900

    config.json
    change_token

--- busline_seongchan.py
+++ busline_seongchan.py
@@ -5,20 +5,27 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from slackclient import SlackClient
 from flask import Flask, request, make_response, render_template
+import time
 
-driver = webdriver.Chrome(r'C:\Users\student\Desktop\chromedriver_win32\chromedriver.exe')
 
-def busline(text):
+def busline(bus_num):
     # 웹사이트 열기
-    driver.get("https://bus.gwangju.go.kr/busmap/lineSearch")
-    bt = driver.find_element_by_css_selector("button.btn.btn-default")  # 클래스만 있는경우
-    bt.click()
+    driver = webdriver.Chrome(r'C:\Users\student\Desktop\chromedriver_win32\chromedriver.exe')
+    url = "https://bus.gwangju.go.kr/busmap/lineSearch"
+    driver.get(url)
 
-    url = "http://www.jobkorea.co.kr/Salary/"
     source = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(source, "html.parser")
-    company = soup.find_all("div", class_="slide")
+    result = soup.find("div", class_="divSearch")
+    # result = soup.find("div", id="contenttabledivGrid")
+    # company = soup.find_all("div", class_="slide")
+    # bt = driver.find_element_by_css_selector("button.btn.btn-default")  # 클래스만 있는경우
+    # bt.click()
+    result.get_text()
+    time.sleep(1000)
+    return
 
-    result = company[i].get_text()[:-10].replace("\n\n\n\n", "\n").replace("\n\n", "\n").replace("\n\n", " ")
+if __name__ == "__main__":
 
-    return result
+    busline(1)
+    pass
