01d1a4b5a41d284eecc65f7701a1e861cc30bcd2
GwonTaeYong <taaii6569@naver.com>
2018-12-20 15:56:58 +0900

    taeyong
    add git ignore

--- input.py
+++ input.py
@@ -1,5 +1,27 @@
+import re
+
+'''
+함수 : input_pre_processing
+매개변수 : user_input (type - string)
+리턴 값 : 0~2(type - integer)
+
+0 = 다시 입력
+1 = 버스노선 검색
+2 = 공지사항 출력
+
+'''
 def input_pre_processing(user_input):
+    numbers = re.findall("\d+", user_input)
 
-    result = 0
+    if len(numbers) > 1:
+        result = 0
+    elif numbers == 1:
+        result = 1
+    else:
+        result = 2
 
-    pass
+    return result
+
+if __name__ == "__main__":
+    input_ = "37번 버스 노선 알려줘111"
+    print(input_pre_processing(input_))
